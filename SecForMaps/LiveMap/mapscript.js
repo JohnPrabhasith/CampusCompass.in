534e42f588b11bca866a94720cc9d3bb207494c3
JohnPrabhasith <johnprabhasith@gmail.com>
2024-01-10 23:17:23 +0530

    v12

--- SecForMaps/LiveMap/mapscript.js
+++ SecForMaps/LiveMap/mapscript.js
@@ -33,7 +33,7 @@ map.addControl(new mapboxgl.NavigationControl());
 map.addControl(
   new mapboxgl.GeolocateControl({
     // positionOptions: {
-    //   enableHighAccuracy: true,
+    // enableHighAccuracy: true,
     // },
     trackUserLocation: true,
   })
@@ -51,7 +51,7 @@ map.on("load", () => {
           type: "Feature",
           properties: {
             description:
-              "<strong>BLOCK 01</strong><p>Make it Mount Pleasant is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>",
+              "<strong>BLOCK 01</strong><p>This block is mainly for the freshers, thier labs, subject faculties and thier staffrooms. Along with those it has the STI hub of the college.</p>",
           },
           geometry: {
             type: "Point",
@@ -62,7 +62,7 @@ map.on("load", () => {
           type: "Feature",
           properties: {
             description:
-              "<strong>CAFETERIA</strong><p>Head to Lounge 201 (201 Massachusetts Avenue NE) Sunday for a Mad Men Season Five Finale Watch Party, complete with 60s costume contest, Mad Men trivia, and retro food and drink. 8:00-11:00 p.m. $10 general admission, $20 admission and two hour open bar.</p>",
+              "<strong>CAFETERIA</strong><p>The place which is loved by every student, where they have their food and make memories for a lifetime with thier friends. The perfect spot for hangouts in college.</p>",
           },
           geometry: {
             type: "Point",
@@ -73,7 +73,7 @@ map.on("load", () => {
           type: "Feature",
           properties: {
             description:
-              "<strong>BLOCK 02</strong><p>EatBar (2761 Washington Boulevard Arlington VA) is throwing a Big Backyard Beach Bash and Wine Fest on Saturday, serving up conch fritters, fish tacos and crab sliders, and Red Apron hot dogs. 12:00-3:00 p.m. $25.</p>",
+              "<strong>BLOCK 02</strong><p>This block is dedicated for the seniors.The floors of this block is divided on the basis of various departments, has their respective department HOD's along with their staffrooms.</p>",
           },
           geometry: {
             type: "Point",
@@ -84,7 +84,7 @@ map.on("load", () => {
           type: "Feature",
           properties: {
             description:
-              "<strong>LIBRARY BLOCK</strong><p>The Ballston Arts & Crafts Market sets up shop next to the Ballston metro this Saturday for the first of five dates this summer. Nearly 35 artists and crafters will be on hand selling their wares. 10:00-4:00 p.m.</p>",
+              "<strong>LIBRARY BLOCK</strong><p>The block contains library which has vast collections of various books,volumes and magzines on different topics, it also has the digital library of the college.Along with it has Yoga hall and GYM for students to maintain their health fitness.It has the CEER dept of the college too.</p>",
           },
           geometry: {
             type: "Point",
@@ -95,7 +95,7 @@ map.on("load", () => {
           type: "Feature",
           properties: {
             description:
-              "<strong>NEW BLOCK</strong><p>Feeling dandy? Get fancy, grab your bike, and take part in this year's Seersucker Social bike ride from Dandies and Quaintrelles. After the ride enjoy a lawn party at Hillwood with jazz, cocktails, paper hat-making, and more. 11:00-7:00 p.m.</p>",
+              "<strong>NEW BLOCK</strong><p>This block is assigned for the CSE specialised branches. It has thier classrooms and thier respective department staffrooms.</p>",
           },
           geometry: {
             type: "Point",
@@ -106,7 +106,7 @@ map.on("load", () => {
           type: "Feature",
           properties: {
             description:
-              "<strong>PHYSICAL EDUCATION ROOM</strong><p>The annual Capital Pride Parade makes its way through Dupont this Saturday. 4:30 p.m. Free.</p>",
+              "<strong>SPORTS ROOM</strong><p>The room has various indoor games for the players to practice and also the outdoor sports kit.The students can use the kits to improve their skills and take part in various competitions.</p>",
           },
           geometry: {
             type: "Point",
@@ -117,7 +117,7 @@ map.on("load", () => {
           type: "Feature",
           properties: {
             description:
-              "<strong>PLAYGROUND-01</strong><p>Jazz-influenced hip hop artist Muhsinah plays the Black Cat (1811 14th Street NW) tonight with Exit Clov and Gods’illa. 9:00 p.m. $12.</p>",
+              "<strong>PLAYGROUND-01</strong><p>This playground has the basketball, throwball, volleyball, kabbadi and tennikoit courts for the students to play and practice.</p>",
           },
           geometry: {
             type: "Point",
@@ -128,7 +128,7 @@ map.on("load", () => {
           type: "Feature",
           properties: {
             description:
-              "<strong>PLAYGROUND-02</strong><p>The Arlington Players' production of Stephen Sondheim's <em>A Little Night Music</em> comes to the Kogod Cradle at The Mead Center for American Theater (1101 6th Street SW) this weekend and next. 8:00 p.m.</p>",
+              "<strong>PLAYGROUND-02</strong><p>This ares is decidated for the cricket, football, handball, kho-kho and softball teams of the college for practicing.</p>",
           },
           geometry: {
             type: "Point",
